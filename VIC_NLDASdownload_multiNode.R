5351337ac1e8623232235ec423524328f4680139
timothycline <tcline@usgs.gov>
2022-01-10 06:54:01 -0700

    add NLDAS download script

---
+++ VIC_NLDASdownload_multiNode.R
@@ -0,0 +1,29 @@
+library(here)
+library(dplyr)
+library(doParallel)
+library(foreach)
+
+DLlist <- read.delim(here('NLDASdownload_Hourly_01062022.txt'),header=F,stringsAsFactors = F) %>% pull(1)
+
+NumNodes <- 5#as.numeric(Sys.getenv('SLURM_JOB_NUM_NODES')) #Get the number of nodes assigned to the job
+taskID <- 1#as.numeric(Sys.getenv('SLURM_PROCID')) #Get the Node number
+dirsplit <- split(DLlist,1:NumNodes) #split total number of directories into groups by
+task_dirlist <- dirsplit[[taskID + 1]] 
+length(task_dirlist)
+
+cl <- makeCluster(detectCores())
+registerDoParallel(cl)
+
+foreach(ff=1:length(task_dirlist)) %dopar% {
+  system("cd ~/Annual_VIC")
+  system("touch .netrc")
+  system('echo "machine urs.earthdata.nasa.gov login timothy_cline password %#%earthdata1542gnpMT" >> .netrc')
+  system("touch .urs_cookies")
+  system(paste0("wget --load-cookies ~/.urs_cookies --save-cookies ~/.urs_cookies --auth-no-challenge=on --no-check-certificate --keep-session-cookies ",task_dirlist[ff]))
+}
+
+stopCluster()
+
+
+
+#https://hydro1.gesdisc.eosdis.nasa.gov/data/NLDAS/NLDAS_VIC0125_H.002/1979/002/NLDAS_VIC0125_H.A19790102.0000.002.grb
