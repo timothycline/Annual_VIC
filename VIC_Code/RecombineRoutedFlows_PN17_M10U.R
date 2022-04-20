2dcd574a80daf472395225f7c664d284755d3f41
timothycline <tcline@usgs.gov>
2022-04-20 12:16:19 -0600

    Recombine by COMID for 2 regions

---
+++ VIC_Code/RecombineRoutedFlows_PN17_M10U.R
@@ -0,0 +1,76 @@
+rm(list=ls())
+
+suppressPackageStartupMessages(library(here))
+suppressPackageStartupMessages(library(dplyr))
+suppressPackageStartupMessages(library(stringr))
+suppressPackageStartupMessages(library(lubridate))
+suppressPackageStartupMessages(library(doParallel))
+suppressPackageStartupMessages(library(foreach))
+
+#Process multiple regions
+regions <- c('PN17','M10U')
+
+rr <- as.numeric(Sys.getenv('SLURM_PROCID')) + 1 #Get the Node number, SLURM returns 0 for first node, but we want 1 for indexing
+rname <- regions[rr]
+
+dir.create(here('NLDASdata','Routed_ByCOMID',rname))
+
+AllRoutedFlows <- list.files(here('NLDASdata','Routed_ByDate',rname))
+SplitNames <- str_split(AllRoutedFlows,pattern='_')
+head(AllRoutedFlows)
+head(SplitNames)
+length(AllRoutedFlows)
+
+#DoubleCheck the regions match
+err1<-lapply(SplitNames,FUN=function(x,rrname=rname){
+  if(x[1] != rrname){print('Error: Non-matching region names')}
+})
+
+#Load all flow files into the environment (~ 20 GB of data to hold in memory!)
+temp1 <- lapply(AllRoutedFlows,FUN=function(ff){
+  assign(x=ff,value=readRDS(here('NLDASdata','Routed_ByDate',rname,ff)), envir = .GlobalEnv)
+})
+rm(temp1)
+
+if(sum(is.na(match(AllRoutedFlows,ls()))) >0 ) print('Error: Not all files loaded into global environment properly')
+
+#Apply over comids
+#combine daily time series
+#Add date labels
+#Save 1 RDS per COMID
+#cl <- makeCluster(detectCores())
+#registerDoParallel(cl)
+
+COMIDS <- names(get(AllRoutedFlows[1]))
+
+COMIDfiles_written <- lapply(COMIDS,FUN=function(cc){
+  comid <- cc
+  FullFlowList <- lapply(AllRoutedFlows,FUN=function(ff,comID=comid){
+    #ff <- AllRoutedFlows[1]
+    #comID <- comid
+    g1<-get(ff)
+    flowvals<-g1[[which(names(g1)==comID)]]
+    return(flowvals)
+  }) %>% bind_rows()
+  
+  COMID_filename <- paste0('COMID_',comid,'_',paste0(str_split(min(FullFlowList$Date),pattern='-')[[1]],collapse=''),'_',paste0(str_split(max(FullFlowList$Date),pattern='-')[[1]],collapse=''),'.RDS')
+  saveRDS(FullFlowList,here('NLDASdata','Routed_ByCOMID',rname,COMID_filename))
+  COMID_filename
+})
+
+# COMIDfiles_written <- foreach(cc = 1:length(COMIDS), .packages = c('dplyr')) %dopar% {
+#   comid <- COMIDS[cc]
+#   FullFlowList <- lapply(AllRoutedFlows,FUN=function(ff,comID=comid){
+#     #ff <- AllRoutedFlows[1]
+#     #comID <- comid
+#     g1<-get(ff)
+#     flowvals<-g1[[which(names(g1)==comID)]]
+#     return(flowvals)
+#   }) %>% bind_rows()
+#   
+#   COMID_filename <- paste0('COMID_',comid,'_',paste0(str_split(min(FullFlowList$Date),pattern='-')[[1]],collapse=''),'_',paste0(str_split(max(FullFlowList$Date),pattern='-')[[1]],collapse=''),'.RDS')
+#   saveRDS(FullFlowList,here('NLDASdata','Routed_ByCOMID',rname,COMID_filename))
+#   COMID_filename
+# }
+
+#stopCluster(cl)
